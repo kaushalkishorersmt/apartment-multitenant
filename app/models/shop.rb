0adce2b0472cbe6b46c940a36c966fddb0556842
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2018-02-12 16:47:09 +0530

    Setup apartment gem

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,11 +1,17 @@
 class Shop < ApplicationRecord
   belongs_to :theme
   belongs_to :main_product
-  
+
+  after_create :create_tenant
+
   has_many :shop_site_admin
   has_many :site_admin, through: :shop_site_admins
 
   before_save do
     self.subdomain = self.shop_name.parameterize if self.shop_name_changed?
+  end
+
+  def create_tenant
+    Apartment::Tenant.create(subdomain)
   end
 end
