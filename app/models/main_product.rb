b8b05493e53886d0b303ed23387faec474332636
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2018-02-12 15:15:11 +0530

    Add main_prouct scaffold

---
+++ app/models/main_product.rb
@@ -0,0 +1,2 @@
+class MainProduct < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212094043) do
+ActiveRecord::Schema.define(version: 20180212094404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "main_products", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "site_admins", force: :cascade do |t|
     t.string "full_name", null: false
