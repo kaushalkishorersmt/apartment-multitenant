ab90cfdba175c6b49c37d4a091ae29ad3df2598c
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2018-02-13 17:52:27 +0530

    Add themes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
+
   def index
+    case request.subdomain
+    when '', 'www'
+
+    else
+      if current_site_admin.present?
+
+      else
+        @products = Product.all
+      end
+    end
   end
+
 end
