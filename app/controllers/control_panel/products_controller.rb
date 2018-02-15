9309aecb1c5fcbb837c42721b8fa9f63e294ceb0
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2018-02-15 16:53:51 +0530

    Add home and customer views in themes.

--- app/controllers/control_panel/products_controller.rb
+++ app/controllers/control_panel/products_controller.rb
@@ -57,7 +57,7 @@ class ControlPanel::ProductsController < ApplicationController
       if @product.update(product_params)
         # byebug
         if @product.is_private == false
-           CommunityProduct.update_all(title: @product.title, decrption: @product.decrption, min_price: @product.min_price, reseller_price: @product.reseller_price, price: @product.price, tax_rate: @product.tax_rate, is_tax_inclusive: @product.is_tax_inclusive, is_featured: @product.is_featured, is_private: @product.is_private, is_community_product: @product.is_community_product, subcategory_id: @product.subcategory_id, image: @product.image, source: Apartment::Tenant.current, source_product_id: @product.id )
+           CommunityProduct.update_all(title: @product.title, decrption: @product.decrption, min_price: @product.min_price, reseller_price: @product.reseller_price, price: @product.price, tax_rate: @product.tax_rate, is_tax_inclusive: @product.is_tax_inclusive, is_featured: @product.is_featured, is_private: @product.is_private, is_community_product: @product.is_community_product, subcategory_id: @product.subcategory_id, product_segment_id: @product.product_segment_id, category_id: @product.category_id, quantity: @product.quantity, image: @product.image, source: Apartment::Tenant.current, source_product_id: @product.id )
 
            @community_product = CommunityProduct.find_by(source: Apartment::Tenant.current, source_product_id: @product.id)
 
